5aecd82d114f5372738dfdcb50346b071af70d38
DeathsAngel <lmansgreat@gmail.com>
2022-06-28 21:16:11 -0400

    Add project files.

---
+++ MovieManagementAPI/Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieManagementAPI.Models;
+using System.Linq;
+using System;
+
+namespace MovieManagementAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        readonly MovieDataContext _movieDataContext;
+
+        public HomeController(MovieDataContext movieDataContext)
+        {
+            _movieDataContext = movieDataContext;
+        }
+
+        [HttpGet]
+        public IActionResult getList()
+        {
+            return Ok(_movieDataContext.Movies);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult getSingle(int id)
+        {
+            try
+            {
+                return Ok(_movieDataContext.Movies.Find(id));
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Create(Movies movie)
+        {
+            try
+            {
+                _movieDataContext.Add(movie);
+
+                _movieDataContext.SaveChanges();
+
+                return Ok();
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+
+        [HttpPut]
+        public IActionResult Edit(Movies movie)
+        {
+            try
+            {
+                _movieDataContext.Movies.Update(movie);
+
+                _movieDataContext.SaveChanges();
+
+                return Ok();
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(Movies movie)
+        {
+            try
+            {
+                _movieDataContext.Movies.Remove(movie);
+
+                _movieDataContext.SaveChanges();
+
+                return Ok();
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+    }
+}
---
+++ MovieManagementAPI/Models/MovieDataContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace MovieManagementAPI.Models
+{
+    public partial class MovieDataContext : DbContext
+    {
+        public MovieDataContext()
+        {
+        }
+
+        public MovieDataContext(DbContextOptions<MovieDataContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Movies> Movies { get; set; }
+
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+//                optionsBuilder.UseSqlServer("data source=DESKTOP-R8FV019\\SQLEXPRESS;initial catalog=MovieData;trusted_connection=true");
+//            }
+//        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movies>(entity =>
+            {
+                entity.ToTable("movies");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Description)
+                    .HasColumnName("description")
+                    .HasMaxLength(500);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasColumnName("name")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Release)
+                    .IsRequired()
+                    .HasColumnName("release")
+                    .HasMaxLength(4)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
