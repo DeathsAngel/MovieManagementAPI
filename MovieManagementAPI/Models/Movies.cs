dd032d8ff41edae1f5f42c28a055f2b574b1c4be
DeathsAngel <lmansgreat@gmail.com>
2022-07-05 11:08:35 -0400

    Required a few things.

--- MovieManagementAPI/Models/Movies.cs
+++ MovieManagementAPI/Models/Movies.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MovieManagementAPI.Models
 {
     public partial class Movies
     {
         public int? Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Description { get; set; }
+        [Required]
         public string Release { get; set; }
     }
 }
